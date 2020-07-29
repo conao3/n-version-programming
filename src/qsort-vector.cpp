4f0b50410a8055e21a8565f7fdfc23ed9a9cd948
conao3 <conao3@gmail.com>
2020-07-29 14:05:27 +0900

    add qsort-vector program

---
+++ src/qsort-vector.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+// https://ja.wikipedia.org/wiki/%E3%82%AF%E3%82%A4%E3%83%83%E3%82%AF%E3%82%BD%E3%83%BC%E3%83%88
+
+int lst[10000];
+
+template <class RandomAccessIterator>
+void quicksort(RandomAccessIterator left, RandomAccessIterator right) {
+  if (distance(left, right) > 0) {
+    RandomAccessIterator i = left, j = right-1;
+    int pivot = *i;
+    while (1) {
+      while (*i < pivot) i++;
+      while (pivot < *j) j--;
+      if (distance(i, j) < 1) break;
+      iter_swap(i, j);
+      i++, j--;
+    }
+    quicksort(left, i);
+    quicksort(j + 1, right);
+  }
+}
+
+int main() {
+  int elm;
+  vector<int> lst;
+
+  while (cin >> elm) lst.push_back(elm);
+  quicksort(lst.begin(), lst.end());
+  for (auto elm: lst) printf("%d ", elm); printf("\n");
+  return 0;
+}
